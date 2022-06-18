54eb5b800b3d1fb4de8f497f0bc17b7266b56de3
Nameless Name <mandriotamark@gmail.com>
2022-06-19 00:19:07 +0200

    some changes in specification

--- imp.go
+++ imp.go
@@ -23,12 +23,12 @@ const (
 	OP_CODE_TROT                       // rotate stack alow-agreat k times
 	OP_CODE_JTYP                       // jump to next type position from 0 k times -> type_table[k]
 	OP_CODE_JCOD                       // jump to next code position from 0 k times -> code_table[k]
-	OP_CODE_IPUT                       // put data k times
-	OP_CODE_OPUT                       // get data k times
-	OP_CODE_AINC                       // increase head element (x) for 1 k times -> x+k
-	OP_CODE_ADEC                       // decrease head element (x) for 1 k times -> x-k
-	OP_CODE_AADD                       // add to head element (x) it self initial value k times -> x*k
-	OP_CODE_ASUB                       // sub from head element (x) it self initial value and increase counter for 1 -> x/k
-	OP_CODE_AMUL                       // multiply head element (x) it self initial value k times -> x^k
-	OP_CODE_AQUO                       // -> log.k(x)
+	OP_CODE_IPUT                       // put data (x) k times
+	OP_CODE_OPUT                       // get data (x) k times
+	OP_CODE_PINC                       // increase head element (x) for 1 k times -> x+k
+	OP_CODE_PDEC                       // decrease head element (x) for 1 k times -> x-k
+	OP_CODE_LINC                       // add to head element (x) it self initial value k times -> x*k
+	OP_CODE_LDEC                       // sub from head element (x)-> x/k
+	OP_CODE_QINC                       // multiply head element (x) it self initial value k times -> x^k
+	OP_CODE_QDEC                       // -> ln(x)/ln(k)
 )
